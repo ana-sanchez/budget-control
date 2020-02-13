1b1e5b6479c4afb655a9e9b5f57615e44c009bc7
ana-sanchez <snchz.rojo@gmail.com>
2020-02-13 14:32:56 +0100

    add docs

---
+++ docs/precache-manifest.3b1537397e5d937319a45e48b81497e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ecd71c5dc14b6e2ec8a0c010280b49",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9bb74988fbbed7af26b3",
+    "url": "./static/css/main.eb8483d0.chunk.css"
+  },
+  {
+    "revision": "4bcd5e2ab85afef0b6c8",
+    "url": "./static/js/2.f9b47eee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.f9b47eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb74988fbbed7af26b3",
+    "url": "./static/js/main.b2245a65.chunk.js"
+  },
+  {
+    "revision": "396560ca8c3c475f33e7",
+    "url": "./static/js/runtime-main.fa5b242a.js"
+  }
+]);
