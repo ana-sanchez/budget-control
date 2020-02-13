3d4ec5b1d4725301c3e731a0c34459b1cfa2981b
ana-sanchez <snchz.rojo@gmail.com>
2020-02-13 12:32:41 +0100

    create component error

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,17 +1,40 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
+import Error from './Error';
 
 
 const Question = () => {
+
+
+  const [quantity, saveQuantity] = useState(0);
+  const [error, saveError] = useState(false);
+
+
+  const makeBudget = e => {
+    saveQuantity(parseInt(e.target.value, 10))
+  }
+
+  const addBudget = e => {
+    e.preventDefault();
+
+    if( quantity < 1 || isNaN(quantity) ) {
+      saveError(true);
+      return
+    } 
+    saveError(false)
+
+  }
+
+
   return ( 
     <Fragment>
-      <h2>
-        Coloca tu presupuesto
-      </h2>
-      <form>
+      <h2>Coloca tu presupuesto</h2>
+      {error ? <Error  message='El presupuesto es incorrecto'/> : null}
+      <form onSubmit={addBudget}>
         <input 
           type='number'
           className='u-full-width'
           placeholder='Coloca tu presupuesto'
+          onChange={makeBudget}
         />
         <input 
           type='submit' 
